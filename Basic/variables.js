cb585167635d1c111998fec9f247da95d933b450
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-11-06 17:15:06 +0000

    variables

--- Basic/variables.js
+++ Basic/variables.js
@@ -1,1 +1,13 @@
-const account
+const accountId = 144553
+let accountEmail = "anikethate2003@gmail.com"
+var accountPass = "12345"
+accountcity = "Pune"
+
+accountEmail = "abc@abc.com"
+accountPass ="212212121"
+accountcity = "mumbai"
+
+// accountId = 1111 not allowed
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPass, accountcity])
