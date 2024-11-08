6f34019719aea83f2b59896f0872792c2752af4d
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-11-08 18:11:26 +0000

    interview datatype

--- Basic/interView_dataTypes.js
+++ Basic/interView_dataTypes.js
@@ -1,6 +1,15 @@
 // Premitive DataTypes
 // String,Number,Boolean,Null,undefined,symbol
+const id = Symbol("123")
 
 // Reference(Non Premitive)
 // Array,oject,Functions
 
+const heros =["shaktiman","gangadhar"]
+
+const myFunction = function(){
+    console.log("Hello")
+}
+
+
+console.log(myFunction());
