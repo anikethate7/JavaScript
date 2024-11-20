87462bfe70761e912206ab3c0df23709ca499129
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-11-20 23:20:08 +0530

    constructor

--- OOP/constructFunc.js
+++ OOP/constructFunc.js
@@ -11,3 +11,13 @@ let mycar = new Car("Toyota", "Camary");
 console.log(mycar);
 let myNewcar = new Car("Tata", "Safari");
 console.log(myNewcar);
+
+function Animal(name){
+    this.name = name,
+    this.describe = function() {
+        return `This is a ${this.name}`;
+    };
+}
+
+let ani = new Animal("Dog")
+console.log(ani.describe())
